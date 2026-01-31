c7ac698c00a635188528fe321cd0f1e485f242e3
Jairus Wong <24001954@myrp.edu.sg>
2026-01-31 23:30:21 +0800

    added cors(?)

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const cors = require("cors");
 const express = require('express');
 const mysql = require('mysql2/promise');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
