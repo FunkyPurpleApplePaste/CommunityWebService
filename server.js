0490f8bdd8bcd4ec32286d7e57e18343068c4ee8
Jairus Wong <24001954@myrp.edu.sg>
2026-01-31 23:42:48 +0800

    removed cors

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const cors = require("cors");
 const express = require('express');
 const mysql = require('mysql2/promise');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
